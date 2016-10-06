cd710e7f4ccb946e330637a77f248ebeb6938e74
Alessandro Candido <candido.ale@gmail.com>
2016-10-06 23:07:30 +0200

    W Lab3!

---
+++ fast_fit.py
@@ -0,0 +1,33 @@
+from pylab import *
+
+###########################################################################
+
+dir=""
+file=""
+
+def f(x, a, b): #funzione da fittare
+    return a+b*x
+
+p0=[1,1]
+
+def Xfun(a,b): #formula per il calcolo della X dalle colonne (a,b) del file in input
+    return a
+def Yfun(a,b): #idem per la Y
+    return b
+    
+def Xerr(a,b,a_err,b_err): #propagazione errore sulle X
+    return a_err
+def Yerr(a,b,a_err,b_err): #propagazione errore sulle Y
+    return b_err
+
+xunit=""
+yunit=""
+
+titolo=""
+Xlab=""
+Ylab=""
+
+fit(dir,file,xunit,yunit,f,p0,titolo,Xlab,Ylab,,Xfun,Yfun,Xerr,Yerr,preplot="true",scarti="true",tabella="true")
+
+###########################################################################
+
