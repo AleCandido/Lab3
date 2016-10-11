7bb35f7919fb42bc539189b30cd327cc1bd3e102
Gnegne <robertoribatti@gmail.com>
2016-10-11 14:24:22 +0200

    srdetfy

---
+++ Esercitazione1/Esercitazione1.py
@@ -0,0 +1,41 @@
+from pylab import *
+sys.path = sys.path +["C:\\Users\\Roberto\\Documents\\GitHub\\Lab3"]
+from analizer import *
+
+###########################################################################
+
+dir= "C:\\Users\\Roberto\\Documents\\GitHub\\Lab3\\Esercitazione1\\"
+file="1KOhm"
+
+def f(x, a, b): #funzione da fittare
+    return a+b*x
+
+p0=[1,1] #parametri iniziali
+
+xunit="volt"
+yunit="volt"
+
+titolo="Grafico $V_{in}$ vs $V_{out}$ per resistenze $\sim 1 K \Omega$"
+Xlab="Tensione $V_{in}$ [$V$]"
+Ylab="Tensione $V_{out}$ [$V$]"
+
+fit(dir,file,xunit,yunit,f,p0,titolo,Xlab,Ylab,table=True,scarti=True)
+
+##########################################################################
+
+directory="C:\\Users\\Roberto\\Documents\\GitHub\\Lab3\\Esercitazione1\\"
+file="4MOhm"
+
+def f(x, a, b): #funzione da fittare
+    return a+b*x
+
+p0=[1,1] #parametri iniziali
+
+xunit="volt"
+yunit="volt"
+
+titolo="Grafico $V_{in}$ vs $V_{out}$ per resistenze $\sim 4 M \Omega$"
+Xlab="Tensione $V_{in}$ [$V$]"
+Ylab="Tensione $V_{out}$ [$V$]"
+
+fit(directory,file,xunit,yunit,f,p0,titolo,Xlab,Ylab,table=True,scarti=True)
