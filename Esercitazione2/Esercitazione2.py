6e59ff5def3943159a0aae576692c27cba364bbe
Alessandro Candido <candido.ale@gmail.com>
2016-10-16 10:55:54 +0200

    ffhgjk

--- Esercitazione2/Esercitazione2.py
+++ Esercitazione2/Esercitazione2.py
@@ -1,7 +1,7 @@
 from pylab import *
-path = "C:\\Users\\Roberto\\Documents\\GitHub\\Lab3\\"
+#path = "C:\\Users\\Roberto\\Documents\\GitHub\\Lab3\\"
 #path = "C:\\Users\\Studenti\\Desktop\\Lab3\\"
-#path = "/home/alessandro/Documents/Università/3°anno/Laboratorio3/Lab3/"
+path = "/home/alessandro/Documents/Università/3°anno/Laboratorio3/Lab3/"
 sys.path = sys.path + [path]
 from analyzer import *
 import uncertainties
