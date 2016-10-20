19019d82faca3b228f0da0b58a7bc692212c478b
Gnegne <robertoribatti@gmail.com>
2016-10-20 21:42:27 +0200

    esr5tyui

--- Esercitazione3/Esercitazione3.py
+++ Esercitazione3/Esercitazione3.py
@@ -32,6 +32,55 @@ fit(dir,file,unit,f,p0,titolo,Xlab,Ylab,XYfun,preplot=True,table=True,tab=tab)
 
 ###########################################################################
 
+#V_EARLY1#
+
+file="Early1r"
+fig="Earlyr"
+R = uncertainties.ufloat(989,mme(989,"ohm"))
+def f(x, a, b):
+    return a*x+b
+
+p0=[1,1]
+
+def XYfun(a):
+    return a[1],(a[0]-a[1])*10e3/R
+
+unit=["volt","volt"]
+
+titolo="$I_C$ vs $V_{CE}$"
+Xlab="Tensione collettore/emettitore $V_{CE}$ [$V$]"
+Ylab="Corrente di collettore $I_C$ [m$A$]"
+tab=["$V_1$","$V_{CE}$"]
+xlimp=[80,150]
+fit(dir,file,unit,f,p0,titolo,Xlab,Ylab,XYfun,preplot=True,table=True,tab=tab,fig=fig,xlimp=xlimp)
+
+###########################################################################
+
+#V_EARLY2#
+
+file="Early2r"
+fig="Earlyr"
+R = uncertainties.ufloat(989,mme(989,"ohm"))
+def f(x, a, b):
+    return a*x+b
+
+p0=[1,1]
+
+def XYfun(a):
+    return a[1],(a[0]-a[1])*10e3/R
+
+unit=["volt","volt"]
+
+titolo="$I_C$ vs $V_{CE}$"
+Xlab="Tensione collettore/emettitore $V_{CE}$ [$V$]"
+Ylab="Corrente di collettore $I_C$ [m$A$]"
+
+tab=["$V_1$","$V_{CE}$"]
+xlimp=[60,120]
+fit(dir,file,unit,f,p0,titolo,Xlab,Ylab,XYfun,preplot=True,table=True,tab=tab,fig=fig,xlimp=xlimp)
+
+###########################################################################
+
 #FAST PLOT I_C VS I_B#
 
 file="ib_var"
