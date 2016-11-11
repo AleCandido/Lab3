4d8d6e1768a8172cdf9531caba1210839e12fc38
Alessandro Candido <candido.ale@gmail.com>
2016-11-11 15:40:16 +0100

    parabola

--- Esercitazione5/Esercitazione5.py
+++ Esercitazione5/Esercitazione5.py
@@ -1,14 +1,14 @@
 from pylab import *
 #path = "C:\\Users\\Roberto\\Documents\\GitHub\\Lab3\\"
-path = "C:\\Users\\Studenti\\Desktop\\Lab3\\"
-#path = "/home/alessandro/Documents/Università/3°anno/Laboratorio3/Lab3/"
+#path = "C:\\Users\\Studenti\\Desktop\\Lab3\\"
+path = "/home/alessandro/Documents/Università/3°anno/Laboratorio3/Lab3/"
 sys.path = sys.path + [path]
 from analyzer import *
 import uncertainties
-dir= path + "Esercitazione5\\"
+dir= path + "Esercitazione5/"
 ###########################################################################
 
-file="nomesensato"
+file="caratterizzazioneJFET"
 
 def f(x, a, b):
     return a+b*x
@@ -20,9 +20,9 @@ def XYfun(a):
 
 unit=["volt","ampere"]
 
-titolo="ciaociao"
-Xlab=""
-Ylab=""
+titolo="Curva caratteristica di gate del JFET"
+Xlab="$V_{GS}$"
+Ylab="$I_D$"
 
 tab=["",""]
 
@@ -30,3 +30,46 @@ fit(dir,file,unit,f,p0,titolo,Xlab,Ylab,XYfun,preplot=True,scarti=True,tab=tab)
 
 ###########################################################################
 
+file="carattx"
+
+def f(x, a, x0):
+    return a*(x-x0)
+
+p0=[1,1]
+
+def XYfun(a):
+    return a[0],a[1]
+
+unit=["volt","ampere"]
+
+titolo="Curva caratteristica di gate del JFET"
+Xlab="$V_{GS}$"
+Ylab="$I_D$"
+
+tab=["",""]
+
+fit(dir,file,unit,f,p0,titolo,Xlab,Ylab,XYfun,preplot=True,scarti=True,tab=tab)
+
+###########################################################################
+
+file="carattx2"
+
+def f(x, a, b, x0):
+    return a*(x - x0)**2 + b
+
+p0=[1,1,1]
+
+def XYfun(a):
+    return a[0],a[1]
+
+unit=["volt","ampere"]
+
+titolo="Curva caratteristica di gate del JFET"
+Xlab="$V_{GS}$"
+Ylab="$I_D$"
+
+tab=["",""]
+
+fit(dir,file,unit,f,p0,titolo,Xlab,Ylab,XYfun,preplot=True,scarti=True,tab=tab)
+
+###########################################################################
