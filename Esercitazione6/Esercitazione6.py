2c37cf948c6c0557f16bdb826e7245ee7defd481
Gnegne <robertoribatti@gmail.com>
2016-11-14 21:30:07 +0100

    rtyrewqerrty

---
+++ Esercitazione6/Esercitazione6.py
@@ -0,0 +1,32 @@
+from pylab import *
+#path = "C:\\Users\\Roberto\\Documents\\GitHub\\Lab3\\"
+path = "C:\\Users\\Studenti\\Desktop\\Lab3\\"
+#path = "/home/alessandro/Documents/Università/3°anno/Laboratorio3/Lab3/"
+sys.path = sys.path + [path]
+from analyzer import *
+import uncertainties
+dir= path + "Esercitazione5\\"
+###########################################################################
+
+file="nomesensato"
+
+def f(x, a, b):
+    return a+b*x
+
+p0=[1,1]
+
+def XYfun(a):
+    return a[0],a[1]
+
+unit=["volt","ampere"]
+
+titolo="ciaociao"
+Xlab=""
+Ylab=""
+
+tab=["",""]
+
+fit(dir,file,unit,f,p0,titolo,Xlab,Ylab,XYfun,preplot=True,scarti=True,tab=tab)
+
+###########################################################################
+
