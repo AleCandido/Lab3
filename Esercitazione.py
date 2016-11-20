95de1e58cb29be7fc2a7e1e0b52f78c796b7c20f
AleCandido <candido.ale@gmail.com>
2016-11-20 10:17:44 +0100

    Minor updates (Esercitazione.py)

--- Esercitazione.py
+++ Esercitazione.py
@@ -1,11 +1,17 @@
 from pylab import *
-#path = "C:\\Users\\Roberto\\Documents\\GitHub\\Lab3\\"
-#path = "C:\\Users\\Studenti\\Desktop\\Lab3\\"
-#path = "/home/alessandro/Documents/Università/3°anno/Laboratorio3/Lab3/"
+
+import sys
+if sys.argv[0] == ale
+    path = "/home/alessandro/Documents/Università/3°anno/Laboratorio3/Lab3/"
+elif sys.argv[0] == lab
+    path = "C:\\Users\\Studenti\\Desktop\\Lab3\\"
+else
+    path = "C:\\Users\\Roberto\\Documents\\GitHub\\Lab3\\"
 sys.path = sys.path + [path]
+dir= path + "Esercitazione/"
+
 from analyzer import *
 import uncertainties
-dir= path + "Esercitazione/"
 ###########################################################################
 
 file=""
@@ -18,7 +24,7 @@ p0=[1,1]
 def XYfun(a):
     return a[0],a[1]
 
-unit=["",""]
+unit=[("",""),("","")]
 
 titolo=""
 Xlab=""
